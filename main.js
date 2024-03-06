e799f132068a521542a67f6cddb48a7691a38bdd
Ammad Ali <aaammadali376@gmail.com>
2024-03-06 09:00:27 +0500

    first assignment

---
+++ main.js
@@ -0,0 +1,2 @@
+var myName = "Ammad";
+console.log("Hellow " + myName);
---
+++ main.ts
@@ -0,0 +1,2 @@
+let myName:string="Ammad"; //make variable by strong typing
+console.log("Hellow "+myName);//print variable,also use concatenation
